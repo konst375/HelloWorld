df1044577799d948b921330ea5757b226f0ee297
KonstantinChirko <kostyachirko375@gmail.com>
2022-09-18 15:56:59 +0300

    codewars.com. A new Kata for sorting odd array elements

---
+++ src/codewarsTasks/SortTheOdd.java
@@ -0,0 +1,26 @@
+package codewarsTasks;
+
+import java.util.Arrays;
+
+public class SortTheOdd {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortArray(new int[]{7, 1})));
+    }
+
+    public static int[] sortArray(int[] array) {
+        int buffer;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                buffer = array[i];
+                for (int j = i; j < array.length; j++) {
+                    if (array[j] % 2 != 0 && buffer > array[j]) {
+                        array[i] = array[j];
+                        array[j] = buffer;
+                        buffer = array[i];
+                    }
+                }
+            }
+        }
+        return array;
+    }
+}

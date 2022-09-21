f2eefc6cfa2cc3184a98468a1322ee67cbc6338b
KonstantinChirko <kostyachirko375@gmail.com>
2022-09-21 08:51:29 +0300

    created more than one thread

---
+++ src/basics/multitheading/MoreThreads.java
@@ -0,0 +1,22 @@
+package basics.multitheading;
+
+public class MoreThreads {
+    public static void main(String[] args) {
+        MyThread myThread1 = MyThread.createAndStart("thread 1");
+        MyThread myThread2 = MyThread.createAndStart("thread 2");
+        MyThread myThread3 = MyThread.createAndStart("thread 3");
+        
+        try {
+            myThread1.getThread().join();
+            System.out.println("thread1 joined");
+            myThread2.getThread().join();
+            System.out.println("thread2 joined");
+            myThread3.getThread().join();
+            System.out.println("thread3 joined");
+        } catch (InterruptedException e) {
+            System.out.println("main thread interrupted, message: " + e.getMessage());
+        }
+
+        System.out.println("Main thread completed");
+    }
+}
--- src/basics/multitheading/MyThread.java
+++ src/basics/multitheading/MyThread.java
@@ -1,23 +1,28 @@
 package basics.multitheading;
 
 public class MyThread implements Runnable {
-    private final Thread thrd;
+    private final Thread thread;
 
     private MyThread(String threadName) {
-        thrd = new Thread(this , threadName);
+        thread = new Thread(this, threadName);
     }
 
-    public static void createAndStart(String threadName) {
+    public static MyThread createAndStart(String threadName) {
         MyThread newThread = new MyThread(threadName);
-        newThread.thrd.start();
+        newThread.thread.start();
+        return newThread;
+    }
+
+    public Thread getThread() {
+        return thread;
     }
 
     @Override
     public void run() {
-        System.out.println(thrd.getName() + "thread launched");
+        System.out.println(thread.getName() + "thread " + thread.getName() + " launched");
         for (int i = 0; i < 10; i++) {
-            System.out.println("At " + thrd.getName() + " counter " + i);
-            System.out.println("Thread completed");
+            System.out.println("At " + thread.getName() + " counter " + i);
         }
+        System.out.println(thread.getName() + "completed");
     }
 }

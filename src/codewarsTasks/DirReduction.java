663da160b3c7d88d312bd592ff3874ba61ea5db0
KonstantinChirko <kostyachirko375@gmail.com>
2022-09-22 21:59:50 +0300

    codewars.com. New task: reduced the sequence of directions

---
+++ src/codewarsTasks/DirReduction.java
@@ -0,0 +1,55 @@
+package codewarsTasks;
+
+import java.util.*;
+
+public class DirReduction {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(dirReduction(new String[]{"EAST", "EAST", "WEST", "NORTH", "WEST", "EAST", "EAST", "SOUTH", "NORTH", "WEST"})));
+    }
+
+    public static String[] dirReduction(String[] arr) {
+        int x = 0;
+        int y = 0;
+
+        for (String currentReduction : arr) {
+            switch (currentReduction) {
+                case "NORTH" -> x++;
+                case "SOUTH" -> x--;
+                case "WEST" -> y++;
+                case "EAST" -> y--;
+            }
+        }
+        String[] result = new String[Math.abs(x) + Math.abs(y)];
+        for (int i = 0; i < result.length; i++) {
+            for (String currentReduction : arr) {
+                switch (currentReduction) {
+                    case "NORTH" -> {
+                        if (x > 0) {
+                            result[i] = "NORTH";
+                            x--;
+                        }
+                    }
+                    case "SOUTH" -> {
+                        if (x < 0) {
+                            result[i] = "SOUTH";
+                            x++;
+                        }
+                    }
+                    case "WEST" -> {
+                        if (y > 0) {
+                            result[i] = "WEST";
+                            y++;
+                        }
+                    }
+                    case "EAST" -> {
+                        if (y < 0) {
+                            result[i] = "EAST";
+                            y--;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}

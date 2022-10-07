0c084527f32576fd2bbef25ab467e3549df349d6
KonstantinChirko <kostyachirko375@gmail.com>
2022-10-07 14:59:32 +0300

    code refactoring
    code refactoring

--- src/codewarsTasks/matrices/determinant/MatrixDeterminant.java
+++ src/codewarsTasks/matrices/determinant/MatrixDeterminant.java
@@ -3,8 +3,10 @@ package codewarsTasks.matrices.determinant;
 public class MatrixDeterminant {
     public static double det(double[][] A) {
         int length = A.length;
+
         if (length == 2)
             return A[0][0] * A[1][1] - A[1][0] * A[0][1];
+
         double[] line = A[0];
         double det = 0;
         for (int i = 0; i < line.length; i++) {
@@ -19,10 +21,7 @@ public class MatrixDeterminant {
         double[][] a = new double[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (j >= colum)
-                    a[i][j] = A[i + 1][j + 1];
-                else
-                    a[i][j] = A[i + 1][j];
+                a[i][j] = (j >= colum) ? A[i + 1][j + 1] : A[i + 1][j];
             }
         }
         return a;
